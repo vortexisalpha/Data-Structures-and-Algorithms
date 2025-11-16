455ca67d96d09750b8319d6bfdacab3079af4693
Josh Hirschkorn <53196448+vortexisalpha@users.noreply.github.com>
2025-11-16 15:12:55 +0000

    Time: 3 ms (83.14%) | Memory: 19.7 MB (5.22%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,13 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        dic = {}
-        size = len(nums)
-        for n in nums:
-            if n not in dic:
-                dic[n] = 1
-            else:
-                dic[n] += 1
-
-        for k,v in dic.items():
-            if size//2 < v:
-                return k
+        dic = collections.Counter(nums)
+        return max(dic.keys(), key=dic.get)
