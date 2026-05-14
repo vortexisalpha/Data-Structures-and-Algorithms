db24c388fd121cc97779f542010796c857f57921
Josh Hirschkorn <53196448+vortexisalpha@users.noreply.github.com>
2026-05-14 15:54:54 +0200

    Time: 107 ms (5.26%) | Memory: 23.4 MB (11.72%) - LeetSync

---
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+class Solution:
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        time_rem = defaultdict(int)
+        for i, x in enumerate(time):
+            time_rem[x % 60] += 1
+
+        
+        cnt = 0  
+        for i, x in enumerate(time):
+            cnt += time_rem[(60 - (x%60)) % 60]
+            if (60 - (x%60)) % 60 == 30 or (60 - (x%60)) % 60 == 0:
+                cnt -= 1
+
+            print(cnt)
+        return int(cnt/2)
+
+          
+
+
+
+
