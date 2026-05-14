531d2fe8a3d2e607f4958fcf7aabe04b1a22d554
Josh Hirschkorn <53196448+vortexisalpha@users.noreply.github.com>
2026-05-14 12:34:02 +0100

    Time: 3 ms (88.89%) | Memory: 22.6 MB (95.13%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,6 +2,6 @@ from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         cnt = Counter(nums)
-        sol = sorted(list(cnt.items()), key = lambda x: x[1], reverse=True)
+        sol = sorted(cnt.items(), key = lambda x: x[1], reverse=True)
         return [sol[i][0] for i in range(k)]
         
