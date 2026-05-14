20e59a605693477cc8cf4c28cdb10b8edd8049d7
Josh Hirschkorn <53196448+vortexisalpha@users.noreply.github.com>
2026-05-14 11:34:19 +0100

    Time: 21 ms (12.27%) | Memory: 23.8 MB (28.21%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,8 +1,16 @@
-from collections import defaultdict
+from collections import Counter, defaultdict
 class Solution:
     #have a dictionary of sorted word being the key to a list of words
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        solution = defaultdict(list)
+        def hash_gen(s):
+            char_list = [0] * 26
+            for c in s:
+                char_list[ord(c)- ord("a")] += 1
+            return tuple(char_list)
+
+
+        sol = defaultdict(list)
         for s in strs:
-            solution[str(sorted(s))].append(s)
-        return list(solution.values())
+            sol[hash_gen(s)].append(s)
+
+        return list(sol.values())
